724bdc58fda4df571b9db4ec3df0a8b7d3bc7802
Gerardo R. Mendoza Vives <82398732+mevigera22@users.noreply.github.com>
2022-10-22 21:13:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const yesBtn = document.querySelector('#yesBtn');
-
-yesBtn.addEventListener('click',function () {
-    alert('Sabia que ibas a decir que si uwu')
+const yesBtn=document.querySelector('#yesBtn');
+yesBtn.addEventListener('click',function(){
+    alert('Te invitamos a crecer 30 cm mas por favor :p')
 });
-
-const noBtn = document.querySelector('#noBtn');
-
-noBtn.addEventListener('mouseover', function () {
-    const randomX = parseInt(Math.random()*100);
-    const randomY = parseInt(Math.random()*100);
-    noBtn.style.setProperty('top',randomY+'%');
-    noBtn.style.setProperty('left',randomX+'%');
-    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
-})
+const noBtn=document.querySelector('#noBtn');
+noBtn.addEventListener('mouseover',function(){
+    const randomx=parseInt(Math.random()*100);
+    const randomy=parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomy+'%');
+    noBtn.style.setProperty('left',randomx+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomx}%,-${randomy}%)`);
+});
